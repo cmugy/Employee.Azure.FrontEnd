f5f9efed79a8734816e69692650017b92806f61c
Collins Mugarura <collismugy@hotmail.com>
2018-09-01 03:49:03 +0100

    Initial commit

---
+++ employee.js
@@ -0,0 +1,51 @@
+function EmployeeController($scope) {
+    $scope.employees= [{
+        "id": 1,
+        "firstName": "Collins",
+        "lastName": "Mugarura",
+        "salary": 43000,
+        "startDate": "2017-11-09T00:00:00"
+    },
+        {
+            "id": 2,
+            "firstName": "Aisha",
+            "lastName": "Mohamed",
+            "salary": 35000,
+            "startDate": "2015-08-03T00:00:00"
+        },
+        {
+            "id": 3,
+            "firstName": "Sienna",
+            "lastName": "Mugarura",
+            "salary": 100,
+            "startDate": "2010-03-09T00:00:00"
+        },
+        {
+            "id": 4,
+            "firstName": "Serena",
+            "lastName": "Mugarura",
+            "salary": 50,
+            "startDate": "2015-03-14T16:05:12.203"
+        },
+        {
+            "id": 5,
+            "firstName": "Kalisa",
+            "lastName": "Murray",
+            "salary": 0,
+            "startDate": "1900-01-01T00:00:00"
+        },
+        {
+            "id": 7,
+            "firstName": "Steve",
+            "lastName": "Hanley",
+            "salary": 32000,
+            "startDate": "2018-08-31T00:00:00"
+        },
+        {
+            "id": 8,
+            "firstName": "Donald",
+            "lastName": "Punk",
+            "salary": 3200,
+            "startDate": "2018-08-31T00:00:00"
+        }]; //get from api
+}
